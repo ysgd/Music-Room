366fd0c7a09e823a37de018ca6d511988bc217b8
Yaju Shrestha <yajustha11@gmail.com>
2024-03-29 20:58:55 +0545

    created serializers, created models

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
 # Create your views here.
-def main(request):
-  return HttpResponse("Hello")
+
+class RoomView(generics.CreateAPIView):
+  pass
